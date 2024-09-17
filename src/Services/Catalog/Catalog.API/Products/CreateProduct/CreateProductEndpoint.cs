e8048b5df45144ca67dd06b5484cbecae20a7f62
AnastasovMario <marioanastasov5@gmail.com>
2024-09-17 15:13:29 +0300

    Adding and Implementing Carter and Mapster libraries

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,6 +1,40 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductEndpoint
+    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    
+    public record CreateProductResponse(Guid Id);
+
+    public class CreateProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            //Define http post endpoint with Carter and Mapster
+
+            app.MapPost("/products",
+                async (CreateProductRequest request, ISender sender) =>
+            {
+                //We are converting from request to command object
+                //Our mediator is requiring command object in order to trigger our command handler
+                var command = request.Adapt<CreateProductCommand>(); // map to command object
+
+                //Send it using a mediator
+                var result = await sender.Send(command); //Trigger the handler class
+
+                //Create a product response
+                var response = result.Adapt<CreateProductResponse>();
+
+                return Results.Created($"/products/{response.Id}", response);
+            })
+            .WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+                
+        }
     }
 }
