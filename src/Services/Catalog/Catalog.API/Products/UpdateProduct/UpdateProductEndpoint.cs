cd27645fb1452db565dc9a7d45c24f6176991386
AnastasovMario <marioanastasov5@gmail.com>
2024-09-18 14:54:14 +0300

    UpdateProduct functionality

---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -0,0 +1,32 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.UpdateProduct
+{
+    //Parameter names should be exact names as the UpdateProductCommand as the mapster would look for the exact names
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+    public record UpdateProductResponse(bool IsSuccess);
+
+    public class UpdateProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                UpdateProductCommand command = request.Adapt<UpdateProductCommand>();
+
+                var result = await sender.Send(command);
+
+                var response = result.Adapt<UpdateProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("UpdateProduct")
+            .Produces<UpdateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update Product")
+            .WithDescription("Update Product");
+        }
+    }
+}
