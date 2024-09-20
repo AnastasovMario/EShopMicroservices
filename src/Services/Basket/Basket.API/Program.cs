915375ada9a37af597858fd27db9ec0b631a9898
AnastasovMario <marioanastasov5@gmail.com>
2024-09-20 11:17:35 +0300

    Add Behaviours

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-//Addd services to the container
+//Add services to the container
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter(); //1. Add carter service
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>)); //Add validation behaviour from our pipeline
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>)); 
+});
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline
+app.MapCarter(); //2. Then map it
 
 app.Run();
