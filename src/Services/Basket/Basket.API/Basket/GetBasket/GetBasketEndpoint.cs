7ab7544fe383ca08da6b0cbc51dd261833404938
AnastasovMario <marioanastasov5@gmail.com>
2024-09-20 09:54:53 +0300

    GetBasket Functionality added

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketEndpoint.cs
@@ -0,0 +1,28 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketRequest();
+
+    public record GetBasketResponse(ShoppingCart Cart);
+
+    public class GetBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/basket/{userName}", async (string username, ISender sender) =>
+            {
+                //Get the result from the query
+                GetBasketResult result = await sender.Send(new GetBasketQuery(username));
+
+                //Convert the result into a response
+                GetBasketResponse response = result.Adapt<GetBasketResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetBasketResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id")
+            .WithDescription("Get Product By Id");
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Basket.API.Basket.GetBasket
+{
+    public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+
+    public record GetBasketResult(ShoppingCart Cart);
+
+    public class GetBasketQueryHandler
+        : IQueryHandler<GetBasketQuery, GetBasketResult>
+    {
+        public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+        {
+            //TODO: Get basket from database
+
+            return new GetBasketResult(new ShoppingCart("abc"));
+        }
+    }
+}
