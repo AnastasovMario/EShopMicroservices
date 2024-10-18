4bcea23fae71e62e501476a9d3e99479b0ec5e56
AnastasovMario <marioanastasov5@gmail.com>
2024-10-18 11:04:38 +0300

    Create and implement Discounts Database

---
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+  public static class Extensions
+  {
+    public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+    {
+      using var scope = app.ApplicationServices.CreateScope();
+      using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
+      //Applies any pending migrations for the context to the database if the database doesn't already exists
+      dbContext.Database.MigrateAsync();
+
+      return app;
+    }
+  }
+}
