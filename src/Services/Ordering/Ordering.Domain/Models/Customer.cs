1494c2be42c8ff812ccfebcb3da0a7cb703198ef
AnastasovMario <marioanastasov5@gmail.com>
2024-10-19 12:03:14 +0300

    Develop DDD Rich-domain model Entity for Order Entities

--- src/Services/Ordering/Ordering.Domain/Models/Customer.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -1,10 +1,25 @@
-﻿using Ordering.Domain.Abstractions;
-
-namespace Ordering.Domain.Models
+﻿namespace Ordering.Domain.Models
 {
   public class Customer : Entity<CustomerId>
   {
     public string Name { get; private set; } = default!;
     public string Email { get; private set; } = default!;
+
+    //In order to separate entities and value objects for the create responsibilities
+    //we will develop the static create method for each entities under the model folder.
+    public static Customer Create(CustomerId id, string name, string email)
+    {
+      ArgumentException.ThrowIfNullOrWhiteSpace(name);
+      ArgumentException.ThrowIfNullOrWhiteSpace(email);
+
+      var customer = new Customer
+      {
+        Id = id,
+        Name = name,
+        Email = email
+      };
+
+      return customer;
+    }
   }
 }
