5297a5618d09083520f7933486cfc9187c19b1d3
AnastasovMario <marioanastasov5@gmail.com>
2024-10-19 16:38:44 +0300

    Add Domain Events for Orders

--- src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
+++ src/Services/Ordering/Ordering.Domain/Abstractions/IDomainEvent.cs
@@ -6,7 +6,6 @@ namespace Ordering.Domain.Abstractions
   public interface IDomainEvent : INotification
   {
     Guid EventId => Guid.NewGuid();
-
     public DateTime OcurredOn => DateTime.Now;
     public string EventType => GetType().AssemblyQualifiedName;
   }
