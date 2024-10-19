57eb88b61ed9bfa3b572d3bb0f9c067693a38eed
AnastasovMario <marioanastasov5@gmail.com>
2024-10-19 16:22:55 +0300

    Add Order functionality

--- src/Services/Ordering/Ordering.Domain/Models/Order.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -2,6 +2,7 @@
 {
   public class Order : Aggregate<OrderId>
   {
+    //All order item operations should be responsible in the Order Aggregate
     private readonly List<OrderItem> _orderItems = new();
     public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
     //Strongly TYPED ID PATTERN 
@@ -18,5 +19,54 @@
       private set { }
     }
 
+    public static Order Create
+      (OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+      var order = new Order
+      {
+        Id = id,
+        CustomerId = customerId,
+        OrderName = orderName,
+        ShippingAddress = shippingAddress,
+        BillingAddress = billingAddress,
+        Payment = payment,
+        Status = OrderStatus.Pending,
+      };
+
+      order.AddDomainEvent(new OrderCreatedEvent(order));
+
+      return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+    {
+
+      OrderName = orderName;
+      ShippingAddress = shippingAddress;
+      BillingAddress = billingAddress;
+      Payment = payment;
+      Status = OrderStatus.Pending;
+
+
+      AddDomainEvent(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+      ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+      ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+      var orderItem = new OrderItem(Id, productId, quantity, price);  
+      _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+      var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+      if (orderItem != null)
+      {
+        _orderItems.Remove(orderItem);
+      }
+    }
   }
 }
