1d9bad2bf953d244a4a6fc05a5f52195de967f94
AnastasovMario <marioanastasov5@gmail.com>
2024-10-19 10:13:23 +0300

    Clean Architecture Layers and Dependency Injections

---
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services to the container
+
+builder.Services
+  .AddApplicationServices()
+  .AddInfrastructureServices(builder.Configuration)
+  .AddApiServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request Pipeline.
+
+app.Run();
