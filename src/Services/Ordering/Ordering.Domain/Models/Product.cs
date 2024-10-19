ba662d11e4b0b0aeeaef01c4dd303f4e576cd1c5
AnastasovMario <marioanastasov5@gmail.com>
2024-10-19 11:44:00 +0300

    Develop Strongly TypedId

--- src/Services/Ordering/Ordering.Domain/Models/Product.cs
+++ src/Services/Ordering/Ordering.Domain/Models/Product.cs
@@ -2,7 +2,7 @@
 
 namespace Ordering.Domain.Models
 {
-  public class Product : Entity<Guid>
+  public class Product : Entity<CustomerId>
   {
     public string Name { get; private set; } = default!;
     public decimal Price { get; private set; } = default!;
