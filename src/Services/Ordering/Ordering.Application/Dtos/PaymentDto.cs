129ed4746a822e0a2ccae0bc04f2d74c24624623
AnastasovMario <marioanastasov5@gmail.com>
2024-10-26 11:48:54 +0300

    Create Dtos for mapping the commands

---
+++ src/Services/Ordering/Ordering.Application/Dtos/PaymentDto.cs
@@ -0,0 +1,4 @@
+﻿namespace Ordering.Application.Dtos;
+
+//Make sure that Cvv is written in this way, otherwise mapster won't work properly
+public record PaymentDto(string CardName, string CardNumber, string Expiration, string Cvv, int PaymentMethod);
