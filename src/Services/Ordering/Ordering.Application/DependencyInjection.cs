cc2781b39d73b48556363e780713071a7641a5c1
AnastasovMario <marioanastasov5@gmail.com>
2024-10-31 16:50:55 +0200

    Register MassRabbitMQ package into Ordering.Application

--- src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 ﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Messaging.MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -7,7 +9,8 @@ namespace Ordering.Application
   //Static class is needed for this
   public static class DependencyInjection
   {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    public static IServiceCollection AddApplicationServices
+      (this IServiceCollection services, IConfiguration configuration)
     {
       services.AddMediatR(config =>
       {
@@ -15,6 +18,8 @@ namespace Ordering.Application
         config.AddOpenBehavior(typeof(LoggingBehavior<,>));
         config.AddOpenBehavior(typeof(ValidationBehavior<,>));
       });
+
+      services.AddMessageBroker(configuration, Assembly.GetExecutingAssembly());
 
       return services;
     }
