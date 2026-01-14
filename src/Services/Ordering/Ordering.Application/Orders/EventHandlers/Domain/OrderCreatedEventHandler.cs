47c0ceaea165b4cb133c9071fb4d3d273928d29f
AnastasovMario <marioanastasov5@gmail.com>
2026-01-14 16:44:06 +0200

    Publish order Created integration event.

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,12 +1,18 @@
-﻿namespace Ordering.Application.Orders.EventHandlers.Domain
+﻿using MassTransit;
+
+namespace Ordering.Application.Orders.EventHandlers.Domain
 {
-    public class OrderCreatedEventHandler(ILogger<OrderCreatedEventHandler> logger)
-      : INotificationHandler<OrderCreatedEvent>
+  public class OrderCreatedEventHandler
+    (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+    : INotificationHandler<OrderCreatedEvent>
+  {
+    public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
-        public Task Handle(OrderCreatedEvent notification, CancellationToken cancellationToken)
-        {
-            logger.LogInformation($"Domain Event handled: {notification.GetType().Name}");
-            return Task.CompletedTask;
-        }
+      logger.LogInformation($"Domain Event handled: {domainEvent.GetType().Name}");
+
+      var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+      
+      await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken); 
     }
+  }
 }
